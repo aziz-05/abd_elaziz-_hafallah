b7b4e4cd4a8ba1ae7aea9ad04a441e3b1e85f104
aziz-05 <azizhafalah@gmail.com>
2023-10-31 17:47:12 +0100

    changed projects

--- components/Header/HeaderLinks/HeaderLinks.js
+++ components/Header/HeaderLinks/HeaderLinks.js
@@ -3,8 +3,11 @@ import { Button, NavigationLink } from '../../UIKit';
 export default function HeaderLinks() {
   return (
     <>
-      <NavigationLink href='/aboutme'>About Me</NavigationLink>
-      <NavigationLink href='/portfolio'>Portfolio</NavigationLink>
+
+
+
+         <NavigationLink href='/aboutme'>About Me</NavigationLink>
+   <NavigationLink href='/portfolio'>Portfolio</NavigationLink>
       <NavigationLink href='/blog'>Blog</NavigationLink>
       <Button variant='secondary' href={'/ABD ELAZIZ HAFALLAH Resume.pdf'}  target="_blank" download>
         Curriculum Vitae
